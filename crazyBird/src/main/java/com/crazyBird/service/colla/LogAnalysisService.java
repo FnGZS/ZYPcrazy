9e646acb6a540f21c340e6c86274ad44fe5e3c7c
a1162032404 <1162032404@qq.com>
2018-10-27 14:03:34 +0800

    11

--- crazyBird/src/main/java/com/crazyBird/service/colla/LogAnalysisService.java
+++
@@ -1,7 +0,0 @@
-package com.crazyBird.service.colla;
-
-import com.crazyBird.dao.colla.dataobject.LogAnalysisDO;
-
-public abstract interface LogAnalysisService {
-	void addLogAnalysis(LogAnalysisDO logAnalysis);
-}
--- crazyBird/src/main/java/com/crazyBird/validate/extend/AbstractExecutor.java
+++
@@ -1,5 +0,0 @@
-package com.crazyBird.validate.extend;
-
-public abstract class AbstractExecutor {
-    public abstract boolean execute(String input);
-}
