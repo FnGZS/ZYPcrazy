78f6123ecea29f70141e0082186bd9be272a0ae5
zjw875596272 <39759870+zjw875596272@users.noreply.github.com>
2018-12-19 21:17:13 +0800

    login的修改

--- crazyBird/src/main/java/com/crazyBird/service/user/impl/UserLoginServiceImpl.java
+++ crazyBird/src/main/java/com/crazyBird/service/user/impl/UserLoginServiceImpl.java
@@ -55,6 +55,7 @@ public class UserLoginServiceImpl implements UserLoginService {
 		ResponseDO<UserLoginDO> responseDO = new ResponseDO<>();
 		UserDO user = userDao.seletUser(wxUser.getOpenId());
 		if (user != null) {
+			//已绑定
 			UserLoginDO userLoginDO = userLoginDao.seletUserByOpenId(wxUser.getOpenId());
 			if (userLoginDO == null) {
 				userLoginDO = new UserLoginDO();
@@ -68,9 +69,16 @@ public class UserLoginServiceImpl implements UserLoginService {
 			} else {
 				userLoginDO = new UserLoginDO();
 				userLoginDO.setOpenId(wxUser.getOpenId());
+				userLoginDO.setLoginAccount(wxUser.getNickName());
+				userLoginDO.setHeadimgurl(wxUser.getHeadimgurl());
+				userLoginDO.setSex(wxUser.getSex());
 				userLoginDO.setAccessToken(TokenUtils.creatAesStr(user.getSchoolNum()));
 				userLoginDO.setIsBound(Integer.valueOf(1));
 				userLoginDao.update(userLoginDO);
+				user.setUserName(wxUser.getNickName());
+				user.setHeadimgurl(wxUser.getHeadimgurl());
+				user.setSex(wxUser.getSex());
+				userDao.updateUser(user);
 			}
 			responseDO.setDataResult(userLoginDO);
 		} else {
@@ -217,6 +225,8 @@ public class UserLoginServiceImpl implements UserLoginService {
 		if(havePhoneUser.getIsBinding() == 1) {
 			havePhoneUser.setOpenId(userLoginDO.getOpenId());
 			havePhoneUser.setUserName(userLoginDO.getLoginAccount());
+			havePhoneUser.setHeadimgurl(userLoginDO.getHeadimgurl());
+			havePhoneUser.setSex(userLoginDO.getSex());
 			userDao.updateUser(havePhoneUser);
 			userLoginDO.setOpenId(havePhoneUser.getOpenId());
 			userLoginDO.setAccessToken(TokenUtils.creatAesStr(havePhoneUser.getSchoolNum()));
